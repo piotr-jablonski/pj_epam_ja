403556883c6b6ab59fca635013e293b2a65493ae
Piotr Jabłoński <1994piotrjablonski@gmail.com>
2017-05-22 11:16:00 +0200

    Hello

---
+++ src/pj_ja_task/Game.java
@@ -0,0 +1,18 @@
+package pj_ja_task;
+
+import java.util.Scanner;
+
+public class Game {
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	
+	
+	
+	
+	public String [][] array = new String [3][3];
+	
+	
+	
+
+}
--- src/pj_ja_task/main.java
+++ src/pj_ja_task/main.java
@@ -3,6 +3,12 @@ package pj_ja_task;
 public class main {
 
 	public static void main (String[] args){
-		System.out.println("Hello");
+		
+		
+		
+		
+		
+		
+		
 	}
 }
